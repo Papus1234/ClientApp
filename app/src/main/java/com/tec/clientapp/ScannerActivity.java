5924ff9c6549e569aa48713cf1e280f32e0cb6b7
AaronSolera <aaron.smartinez1997@gmail.com>
2016-10-17 20:44:43 -0600

    Actualización
    deedf

--- app/src/main/java/com/tec/clientapp/ScannerActivity.java
+++ app/src/main/java/com/tec/clientapp/ScannerActivity.java
@@ -1,13 +1,13 @@
 package com.tec.clientapp;
-
+import android.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MenuActivity extends AppCompatActivity {
+public class ScannerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_scanner);
     }
 }
