ba570cdef54f80224bae7f3b7ef76982339087bc
DeltaMagnus <vetruviantec@gmail.com>
2016-10-20 22:44:00 -0600

    Usuarios
    Se crearon unas clases para lograr extraer la informacion que ingresa el
    usuario

---
+++ app/src/main/java/com/tec/clientapp/ClientInfo.java
@@ -0,0 +1,19 @@
+package com.tec.clientapp;
+
+/**
+ * Created by James on 20/10/2016.
+ */
+
+public class ClientInfo {
+    Client ClientInfo = new Client();
+    User = (EditText) findViewById( R.id.USER);
+    Ip = (EditText) findViewById( R.id.IP);
+    String AuxIP = Ip.getText().toString();
+    String AuxUser = User.getText().toString();
+
+
+
+
+    ClientInfo.setUser(AuxUser)=
+    ClientInfo.setIP
+}
